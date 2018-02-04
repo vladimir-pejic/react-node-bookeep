841d4812b1695f5dfd583c6f1db743abbf150e4c
vladimir_kompitenz <v.pejic@kompitenz.de>
2018-02-04 22:25:49 +0100

    Redux, async request to server, server and front validation

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Form, Button } from 'semantic-ui-react';
+import { Form, Button, Message } from 'semantic-ui-react';
 import Validator from 'validator';
 import InlineError from '../messages/InlineError';
 
@@ -27,7 +27,10 @@ class LoginForm extends React.Component {
         const errors = this.validate(this.state.data);
         this.setState({ errors });
         if(Object.keys(errors).length === 0) {
-            this.props.submit(this.state.data);
+            this.setState({ loading: true });
+            this.props
+                .submit(this.state.data)
+                .catch(err => this.setState({ errors: err.response.data.errors, loading: false }));
         }
     };
 
@@ -39,16 +42,21 @@ class LoginForm extends React.Component {
     };
 
     render() {
-        const { data, errors } = this.state;
+        const { data, errors, loading } = this.state;
 
         return(
-            <Form onSubmit={this.onSubmit}>
-                <Form.Field errors={!!errors.email}>
+            <Form onSubmit={this.onSubmit} loading={loading}>
+                { errors.global && (
+                <Message negative>
+                    <Message.Header>Danger Vladimir, danger!!!</Message.Header>
+                    <p>{ errors.global }</p>
+                </Message>)}
+                <Form.Field errors={errors.email}>
                     <label htmlFor="email">Email</label>
                     <input type={'email'} name={'email'} placeholder={'example@example.com'} value={data.email} onChange={this.onChange}/>
                     { errors.email && <InlineError text={errors.email}/>}
                 </Form.Field>
-                <Form.Field errors={!!errors.password}>
+                <Form.Field errors={errors.password}>
                     <label htmlFor="password">Password</label>
                     <input type={'password'} name={'password'} placeholder={'Enter Password'} value={data.password} onChange={this.onChange}/>
                     { errors.password && <InlineError text={errors.password}/>}
